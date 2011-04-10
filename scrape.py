7494794574cbe888a495d7fdf467c103206faffd
Sam Kohn <therunner05@gmail.com>
2011-04-09 21:06:14 -0400

    cleaned up scrape.py to get rid of urllib

--- scrape.py
+++ scrape.py
@@ -1,14 +1,9 @@
-import urllib
 import feedparser
 urltoscrape = 'http://bwog.com/feed/'
-x = urllib.urlopen(urltoscrape)
-str1 = x.read()
 d = feedparser.parse(urltoscrape)
-x.close()
-#print str1
-#print d.feed.title
 for entry in d.entries:
     for cat in entry.categories:
         if cat[1]=="free food":
             print entry.title
-        
+            print entry.link
+            print "\n"
