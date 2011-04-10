8d2f8e4aba0022257acd040f76f735c6c84d806b
Sam Kohn <therunner05@gmail.com>
2011-04-09 20:22:04 -0400

    included feedparser in scrape.py

--- scrape.py
+++ scrape.py
@@ -1,6 +1,9 @@
 import urllib
+import feedparser
 urltoscrape = 'http://bwog.com/feed/'
 x = urllib.urlopen(urltoscrape)
 str1 = x.read()
+d = feedparser.parse(urltoscrape)
 x.close()
-print str1
+#print str1
+print d.feed.title
