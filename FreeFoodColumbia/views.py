58a8352555d84685925d4145e771a987abd76844
Mason Silber <mds2161@columbia.edu>
2011-04-09 22:32:04 -0400

    For Kathy

--- FreeFoodColumbia/views.py
+++ FreeFoodColumbia/views.py
@@ -3,10 +3,16 @@ from django.template.loader import get_template
 from django.template import Context
 
 def index(request):
-  name = 'FreeFoodColumiba111'
+  name = 'FreefoodColumbia111'
   t = get_template('trash.tmpl')
   html = t.render(Context({'name':name}))
   return HttpResponse(html)
 
 def hello(request):
   return HttpResponse("Hello World")
+
+def allFood(request):
+	name = 'Test Name'
+	s = get_template('listing.tmpl')
+	html = s.render(Context(('name':name)))
+	return HttpResponse(html)
