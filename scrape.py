9073eddbf8a76d23267819e0430eb949be64386f
Sam Kohn <therunner05@gmail.com>
2011-04-09 20:48:25 -0400

    scrape.py now prints out titles of posts tagged with free food

--- scrape.py
+++ scrape.py
@@ -6,4 +6,9 @@ str1 = x.read()
 d = feedparser.parse(urltoscrape)
 x.close()
 #print str1
-print d.feed.title
+#print d.feed.title
+for entry in d.entries:
+    for cat in entry.categories:
+        if cat[1]=="free food":
+            print entry.title
+        
